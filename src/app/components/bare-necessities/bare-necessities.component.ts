c9e4fb498d00863923f6b6e5d7dcc0683c99c8ef
Erika Valdes <envm92@gmail.com>
2026-01-15 17:04:16 -0800

    Rename all files to add helpful suffix

---
+++ src/app/components/bare-necessities/bare-necessities.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bare-necessities',
+  imports: [],
+  templateUrl: './bare-necessities.component.html',
+  styleUrl: './bare-necessities.component.sass',
+})
+export class BareNecessitiesComponent {
+
+}
---
+++ src/app/components/sensory-internal/sensory-internal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensory-internal',
+  imports: [],
+  templateUrl: './sensory-internal.component.html',
+  styleUrl: './sensory-internal.component.sass',
+})
+export class SensoryInternalComponent {
+
+}
