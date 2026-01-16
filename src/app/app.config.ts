5c698a0312a752b902c0f6ea12595b9e1db6cbb7
Erika Valdes <envm92@gmail.com>
2026-01-15 19:33:40 -0800

    Setup Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+		provideClientHydration(withEventReplay()),
+		provideFirebaseApp(() => initializeApp({
+		})), provideAuth(() => getAuth())
+	]
 };
