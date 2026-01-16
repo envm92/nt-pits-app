8c26d3ec92410129befccef22941892d2286855a
Erika Valdes <envm92@gmail.com>
2026-01-15 19:19:57 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject  } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+	selector: 'app-root',
+	imports: [
+		AsyncPipe,
+		RouterOutlet
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  protected readonly title = signal('nt-pits-app');
+	protected readonly title = signal('nt-pits-app');
 }
