89601737ec3f6bb0e2d5ace69347db7c59d43ecb
Erika Valdes <envm92@gmail.com>
2026-01-21 16:20:58 -0800

    Theme binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component, signal, inject  } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-root',
 	imports: [
-		AsyncPipe,
-		RouterOutlet
-	],
+    AsyncPipe,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+],
 	templateUrl: './app.component.html',
-	styleUrl: './app.component.sass'
+	styleUrl: './app.component.scss'
 })
-export class AppComponent {
-	protected readonly title = signal('nt-pits-app');
+export class AppComponent implements OnInit {
+	public theme = 'light';
+	ngOnInit(): void {
+		this.theme = window.matchMedia?.('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+		console.log(this.theme);
+	}
+	changeTheme(event: PointerEvent): void {
+		this.theme = (this.theme === 'light') ? 'dark' : 'light';
+		console.log(this.theme)
+	}
 }
