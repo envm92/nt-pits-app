eed76bbc1c86bb52dbe06180213e3f08b1874926
Erika Valdes <envm92@gmail.com>
2026-01-21 16:41:48 -0800

    Add error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
-	{ path: '', component: MainComponent}
+	{ path: '', component: MainComponent},
+	{ path: 'error', component: ErrorComponent},
+	{ path: '**', redirectTo: 'error'}
 ];
